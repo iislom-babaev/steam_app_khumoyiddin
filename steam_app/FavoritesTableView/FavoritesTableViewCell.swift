f6ea0e10d8cae18ff51ec24fc0c44126f2e76221
Khumoyiddin <xuma.27.09@gmail.com>
2023-01-09 11:49:33 +0500

    Favorites screen is done

---
+++ steam_app/FavoritesTableView/FavoritesTableViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  FavoritesTableViewCell.swift
+//  steam_app
+//
+//  Created by Khumoyiddin on 09/01/23.
+//
+
+import UIKit
+
+class FavoritesTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    let favsList = LocalCache.favsList
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+    func cellConfiguration(cell: FavoritesTableViewCell, indexPath: IndexPath) {
+        let favs = favsList[indexPath.row]
+        
+        cell.setTitleText(text: favs.title)
+        cell.setPriceText(text: favs.price)
+        cell.setOnDicsountColor(isOnDiscount: favs.isOnDiscount, color: UIColor(named: "GreenColor")!)
+    }
+    
+    func setTitleText(text: String) {
+        titleLabel.text = text
+    }
+    
+    func setPriceText(text: String) {
+        priceLabel.text = text
+    }
+    
+    func setOnDicsountColor(isOnDiscount: Bool, color: UIColor) {
+        if isOnDiscount {
+            priceLabel.textColor = color
+        } else {
+            priceLabel.textColor = .white
+        }
+    }
+}
+
+extension FavoritesTableViewCell {
+    static let nibName: String = "FavoritesTableViewCell"
+}
--- steam_app/GamesTableView/GamesTableViewCell.swift
+++ steam_app/GamesTableView/GamesTableViewCell.swift
@@ -19,8 +19,6 @@ class GamesTableViewCell: UITableViewCell {
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
-        // Configure the view for the selected state
     }
     
     @IBAction func favoriteToggle(_ sender: UIButton) {
