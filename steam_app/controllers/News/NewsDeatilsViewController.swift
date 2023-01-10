8ba6c13c6f3cfcf3d503d7289dfcd842f4e8f96e
Khumoyiddin <xuma.27.09@gmail.com>
2023-01-10 10:49:08 +0500

    News and News Details screen done

---
+++ steam_app/controllers/News/NewsDeatilsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewsDeatilsViewController.swift
+//  steam_app
+//
+//  Created by Khumoyiddin on 09/01/23.
+//
+
+import UIKit
+
+class NewsDeatilsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
