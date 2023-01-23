588bf4c75d3ae30e29745eb9fadf64fb3dadd1a8
Khumoyiddin <xuma.27.09@gmail.com>
2023-01-23 16:48:28 +0500

    in progress

---
+++ steam_app/controllers/TabBarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarViewController.swift
+//  steam_app
+//
+//  Created by Khumoyiddin on 23/01/23.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
